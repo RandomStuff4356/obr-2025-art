f2b8b58709ce43c84aae5e17f91323fb79643a2b
Tiago Nannin <126704545+RandomStuff4356@users.noreply.github.com>
2025-04-25 21:40:38 -0300

    Testes
    -25/04/2025:
    A gradiente amarela do LED funciona como intencionada, porém, o valor da variável leitura não está funcionando direito; Vou testar soluções amanhã.

--- art-torre.ino
+++ art-torre.ino
@@ -30,17 +30,6 @@ void loop()
 {
   if(analogRead(sensorLumen) > 250){ leitura = 1;}
   else{ leitura = 0;}
-  Serial.println(analogRead(sensorLumen));
-  if(status=="stable" && isChecking) 
-  {
-    isChecking = false;
-    lighthouseStable();
-  }
-  else if(status == "panic" && isChecking)
-  {
-    isChecking = false;
-  	lighthousePanic();
-  }
 
   if(leitura == 0 && status == "stable")
  {
@@ -48,22 +37,46 @@ void loop()
     status = "panic";
     isChecking = true;
  }
-}
+ else
+ {
+    //Serial.println(analogRead(sensorLumen));
+    if(status=="stable" && isChecking) 
+    {
+      isChecking = false;
+     lighthouseStable();
+    }
+    else if(status == "panic" && isChecking)
+    {
+      isChecking = false;
+  	  lighthousePanic();
+    }
+  }
+
+ }
 
 void lighthouseStable() {
-  analogWrite(r, 255);
-  analogWrite(g, 255);
+  analogWrite(b, 0);
 
-  for(int i=0; i <= 255; i++) //Gradualmente deixa o amarelo mais claro 
+  for(int i=100; i <= 200; i++) //Gradualmente deixa o amarelo mais claro 
   { 
-    analogWrite(b, i); 
-    //Serial.println(i); 
+    analogWrite(r, i); 
+    analogWrite(g, i); 
+    Serial.println(i);
+    Serial.println(analogRead(sensorLumen));
+    Serial.println(leitura);
+    Serial.println(status);
+    delay(400);//Desacelera a gradiente
   }
   
-    for(int i=255; i>=0; i--) //Gradualmente deixa o amarelo mais escuro
+    for(int i=200; i>=100; i--) //Gradualmente deixa o amarelo mais escuro
   {
-    analogWrite(b, i);
-    //Serial.println(i);
+    analogWrite(r, i); 
+    analogWrite(g, i);
+    Serial.println(i);
+    Serial.println(analogRead(sensorLumen));
+    Serial.println(leitura);
+    Serial.println(status);
+    delay(400);//Desacelera a gradiente
   }
   isChecking = true;
 }
@@ -76,19 +89,15 @@ void lighthousePanic()
   for(int i=255; i <=53; i++) //Gradualmente deixa o vermelho mais claro 
   { 
     analogWrite(r, i);
-    delay(500);
-    analogWrite(r, LOW);
-    delay(500);    
-    //Serial.println(i); 
+    Serial.println(i); 
+    delay(400);//Desacelera a gradiente
   }
     
   for(int i=53; i>=255; i--) //Gradualmente deixa o vermelho mais escuro
   {
     analogWrite(r, i);
-    delay(500);
-    analogWrite(r, LOW);
-    delay(500);
-    //Serial.println(i);
+    Serial.println(i);
+    delay(400);//Desacelera a gradiente
   }
   isChecking = true;
 }
