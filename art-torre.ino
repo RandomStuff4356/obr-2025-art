66b4bba9f996d0ec22a8a90d422e13cdccb9182f
Tiago Nannin <126704545+RandomStuff4356@users.noreply.github.com>
2025-04-27 19:18:35 -0300

    Update 27/04/2025
    A variável leitura deixou de ser utilizada, mas a leitura do sensor não causou mudança no if quando era esperado. Além disso, formatei o arquivo e adicionei comentários para facilitar a compreensão deste.

--- art-torre.ino
+++ art-torre.ino
@@ -24,37 +24,55 @@ void setup()
   pinMode(b, OUTPUT);
 
   Serial.begin(9600);
-}
+}//Fim do setup
 
 void loop() 
 {
-  if(analogRead(sensorLumen) > 250){ leitura = 1;}
-  else{ leitura = 0;}
+  leitura = analogRead(sensorLumen); //Leitura do sensor de luz
 
-  if(leitura == 0 && status == "stable")
- {
-    servo.write(0);//Libera o meteoro
-    status = "panic";
+  if(!isChecking)//Se a variável isChecking for falsa
+  {  
+    if(analogRead(sensorLumen) <= 200 && status == "stable")
+    {
+      servo.write(0);//Libera o meteoro
+      status = "panic";
+    }
+
+    else if(analogRead(sensorLumen) >= 200 && status == "panic")
+    {
+      status = "impact";
+    }
+      
     isChecking = true;
- }
- else
- {
+  }//Final do if do check
+
+  else  //Se isChecking for verdadeira
+  {
     //Serial.println(analogRead(sensorLumen));
-    if(status=="stable" && isChecking) 
+    if(status=="stable") 
     {
       isChecking = false;
-     lighthouseStable();
+      lighthouseStable();
     }
-    else if(status == "panic" && isChecking)
+
+    else if(status == "panic")
     {
       isChecking = false;
   	  lighthousePanic();
     }
-  }
 
- }
+    else if(status == "impact")
+    {
+      isChecking = false;
+      lighthouseImpact();
+    }
 
-void lighthouseStable() {
+  }   //Final do else
+
+}//Fim do loop
+
+void lighthouseStable() 
+{
   analogWrite(b, 0);
 
   for(int i=100; i <= 200; i++) //Gradualmente deixa o amarelo mais claro 
@@ -66,9 +84,9 @@ void lighthouseStable() {
     Serial.println(leitura);
     Serial.println(status);
     delay(400);//Desacelera a gradiente
-  }
+  }//Fim do for
   
-    for(int i=200; i>=100; i--) //Gradualmente deixa o amarelo mais escuro
+  for(int i=200; i>=100; i--) //Gradualmente deixa o amarelo mais escuro
   {
     analogWrite(r, i); 
     analogWrite(g, i);
@@ -77,9 +95,10 @@ void lighthouseStable() {
     Serial.println(leitura);
     Serial.println(status);
     delay(400);//Desacelera a gradiente
-  }
+  }//Fim do for
+
   isChecking = true;
-}
+}//Fim da função
 
 void lighthousePanic() 
 {
@@ -91,13 +110,32 @@ void lighthousePanic()
     analogWrite(r, i);
     Serial.println(i); 
     delay(400);//Desacelera a gradiente
-  }
+  }//Fim do for
     
   for(int i=53; i>=255; i--) //Gradualmente deixa o vermelho mais escuro
   {
     analogWrite(r, i);
     Serial.println(i);
     delay(400);//Desacelera a gradiente
-  }
+  }//Fim do for
+
   isChecking = true;
-}
+}//Fim da função
+
+void lighthouseImpact()
+{
+  for(int i = 8; i != 0; i--)
+  {
+    analogWrite(r, 255);
+    analogWrite(g, 255);
+    analogWrite(b, 255);
+    //LED branco
+    delay(random(100, 1000));//Delay aleatório de 0,1s a 1s
+    analogWrite(r, 0);
+    analogWrite(g, 0);
+    analogWrite(b, 0);
+    //LED desligado
+    delay(random(100, 1000));//Delay aleatório de 0,1s a 1s
+  }//Fim do for
+
+}//Fim da função
