47ec96f0a7ed27ead909baaface6d2ebf7e3763e
Tiago Nannin <126704545+RandomStuff4356@users.noreply.github.com>
2025-05-05 16:17:08 -0300

    fix
    Consertei erros bestas

--- art-montanha.ino
+++ art-montanha.ino
@@ -3,13 +3,17 @@
 String status;
 
 Servo servo;
-void Start()
+void setup()
 {
     servo.attach(9, 500, 2500);
     servo.write(90); //Coloca o servo na posição inicial
     Serial.begin(9600);
 }
-void Loop()
+void loop()
 {
     status = Serial.read();
+    if(status == "panic")
+    {
+        servo.write(0);        
+    }
 }
